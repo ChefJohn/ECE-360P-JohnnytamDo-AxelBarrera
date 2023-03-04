f0e665cfe268a65f434447dcb025a9bea4173043
axebarrera <55003651+axebarrera@users.noreply.github.com>
2023-03-03 18:58:35 -0600

    Added communication methods to ClientHandler

--- UDPClientHandler.java
+++ UDPClientHandler.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.HashMap;
 
 public class UDPClientHandler implements Runnable {
@@ -40,5 +42,25 @@ public class UDPClientHandler implements Runnable {
         //TODO: When set-mode t command comes this should execute the neccesary server changes
     }
 
+    private void sendUDP(String message,
+                                DatagramPacket receivedPack,
+                                DatagramSocket socket)throws Exception {
+        InetAddress address = receivedPack.getAddress();
+        int port = receivedPack.getPort();
+        byte[] data = message.getBytes();
+        DatagramPacket pack = new DatagramPacket(data,data.length,address,port);
+        socket.send(pack);
+    }
 
+    private String extractMessage(DatagramPacket pack){
+        String message = new String(pack.getData(),0, pack.getLength());
+        return message;
+    }
+
+    private DatagramPacket receiveUDP(DatagramSocket socket) throws IOException{
+        byte[] data = new byte[1024];
+        DatagramPacket pack = new DatagramPacket(data,data.length);
+        socket.receive(pack);
+        return pack;
+    }
 }
