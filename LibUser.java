56d4a01418cd2b445e305f380748fa81e6d4385b
Johnny Do <johnnydo@Johnnys-Air.lan>
2023-03-03 19:21:35 -0600

    Implemented the functions for LibUser

--- LibUser.java
+++ LibUser.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class LibUser {
     private String user;
-    private HashMap<String,Integer> bookCountMap;
-    private HashMap<Integer,String> loanBookMap;
+    private HashMap<String,Integer> bookCountMap;       //book name: # of books available
+    private HashMap<Integer,String> loanBookMap;        //loanID: book name
 
     public LibUser(String user, HashMap<String, Integer> bookCountMap) {
         this.user = user;
@@ -12,22 +12,75 @@ public class LibUser {
         this.loanBookMap = new HashMap<>();
     }
 
-    public void userBeginLoan(){
+    public String userBeginLoan(int loanID, String bookName){
         /*TODO: fulfills the begin-loan command. If the request is valid, then this function will map the loan id with
         the book name to the respective user
         */
+
+        String result = "";
+
+        //checking to see if requested book exists
+        if (bookCountMap.get(bookName) == null){
+            result = "Request Failed - We do not have this book";
+            return result;
+        }
+
+        //checking to see if requested book has avilability 
+        if (bookCountMap.get(bookName) == 0){
+            result = "Request Failed - Book not available";
+            return result;
+        }
+
+        //if all checks out, then loan out the book to the respective user
+        loanBookMap.put(loanID, bookName);
+        bookCountMap.put(bookName, bookCountMap.get(bookName) - 1);
+        result = "Your request has been approved, " + loanID + " " + this.user + " " + bookName;
+        return result;
     }
 
-    public void userEndLoan(){
+    public String userEndLoan(int loanID){
         /*TODO: fulfills the end-loan command. If the requests is valid, then this function will unmap the loan id and 
         the book associated with it from the respective user
         */
+
+        String result = "";
+
+        if (loanBookMap.get(loanID) == null){
+            result = loanID + " not found, no such borrow record";
+            return result;
+        }
+
+        //if valid, then give the loan book back
+        loanBookMap.remove(loanID);
+        String bookName = loanBookMap.get(loanID);
+        bookCountMap.put(bookName, bookCountMap.get(bookName) + 1);
+        result = loanID + " is returned";
+        return result;
     }
 
-    public void userGetLoan(){
+    public String[] userGetLoan(){
         /*TODO: fulfills the get-loan command. If the requests is valid, then this function will just return all the loans
         that is associated with the respective user. If not, then just simply return ‘No record found for <user-name>’
         */
+
+        //checking if the respective user has any loaned books
+        if (loanBookMap.isEmpty()){
+            String result = "No record found for " + this.user;
+            String[] resultArr = new String[1];
+            resultArr[0] = result;
+            return resultArr;
+        }
+
+        //if valid then return array of all loans
+        String[] resultArr = new String[loanBookMap.size()];
+        int i = 0;
+        for (Map.Entry<Integer,String> mapElement : loanBookMap.entrySet()) {
+            String result =  mapElement.getKey() + " " + mapElement.getValue();
+            resultArr[i] = result;
+            i++;
+        }
+
+        return resultArr;
     }
 
 }
